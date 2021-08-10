8dca22b751c29b286a8d41af986582869e599c23
Balázs Barkó <Balazs.Barko@oneidentity.com>
2021-08-10 15:22:56 +0200

    stats: add new tag
    * largest_size which will contain the largest in coming/out going message sizes
    
    Signed-off-by: Balázs Barkó <Balazs.Barko@oneidentity.com>

--- lib/stats/stats-cluster-logpipe.c
+++ lib/stats/stats-cluster-logpipe.c
@@ -37,6 +37,8 @@ static const gchar *tag_names[SC_TYPE_MAX] =
   /* [SC_TYPE_MATCHED] = */ "matched",
   /* [SC_TYPE_NOT_MATCHED] = */ "not_matched",
   /* [SC_TYPE_WRITTEN] = */ "written",
+  /* [SC_TYPE_LARGEST] = */ "largest_size",
+  /* [SC_TYPE_AVG] = */ "average_size",
 };
 
 static void
